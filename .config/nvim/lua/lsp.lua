2ac0bce7681590de54436ac21ce1b8717a9f0224
theoboldalex <theoboldalex@gmail.com>
2022-12-09 19:44:26 +0000

    forgot gopls

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -73,16 +73,7 @@ nvim_lsp.rust_analyzer.setup({
 
 nvim_lsp.gopls.setup {
     capabilities = capabilities,
-    on_attach = function()
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, {buffer = 0})
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {buffer = 0})
-        vim.keymap.set('n', 'gr', vim.lsp.buf.references, {buffer = 0})
-        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {buffer = 0})
-        vim.keymap.set('n', '<leader>dj', vim.diagnostic.goto_next, {buffer = 0})
-        vim.keymap.set('n', '<leader>dk', vim.diagnostic.goto_prev, {buffer = 0})
-        vim.keymap.set('n', '<leader>df', '<cmd>Telescope diagnostics<cr>', {buffer = 0})
-        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {buffer = 0})
-    end
+    on_attach = on_attach
 }
 
 local cmp = require'cmp'
