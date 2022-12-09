4539d8270abddc27a1c3b57de32e93da31aa151b
theoboldalex <theoboldalex@gmail.com>
2022-12-09 19:43:11 +0000

    whitespace

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -11,16 +11,16 @@ vim.cmd('hi DiagnosticInfo guifg=#3399ff')
 vim.cmd('hi DiagnosticHint guifg=#3399ff')
 
 local on_attach = function()
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, {buffer = 0})
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {buffer = 0})
-        vim.keymap.set('n', 'gr', vim.lsp.buf.references, {buffer = 0})
-        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {buffer = 0})
-        vim.keymap.set('n', '<leader>dj', vim.diagnostic.goto_next, {buffer = 0})
-        vim.keymap.set('n', '<leader>dk', vim.diagnostic.goto_prev, {buffer = 0})
-        vim.keymap.set('n', '<leader>df', '<cmd>Telescope diagnostics<cr>', {buffer = 0})
-        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {buffer = 0})
-        vim.keymap.set('n', '<C-h>', vim.lsp.buf.signature_help, {buffer = 0})
-    end
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, {buffer = 0})
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {buffer = 0})
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, {buffer = 0})
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {buffer = 0})
+    vim.keymap.set('n', '<leader>dj', vim.diagnostic.goto_next, {buffer = 0})
+    vim.keymap.set('n', '<leader>dk', vim.diagnostic.goto_prev, {buffer = 0})
+    vim.keymap.set('n', '<leader>df', '<cmd>Telescope diagnostics<cr>', {buffer = 0})
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {buffer = 0})
+    vim.keymap.set('n', '<C-h>', vim.lsp.buf.signature_help, {buffer = 0})
+end
 
 nvim_lsp.intelephense.setup {
     capabilities = capabilities,
