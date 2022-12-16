b434f0131e2d27161a3841203ab8e11ec91d6697
theoboldalex <theoboldalex@gmail.com>
2022-12-16 21:32:28 +0000

    cleanup

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -14,23 +14,22 @@ local on_attach = function ()
         virtual_text = true,
     })
 
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', '<leader>dj', vim.diagnostic.goto_next, opts)
-    vim.keymap.set('n', '<leader>dk', vim.diagnostic.goto_prev, opts)
-    vim.keymap.set('n', '<leader>df', '<cmd>Telescope diagnostics<cr>', opts)
-    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-    vim.keymap.set('n', '<C-h>', vim.lsp.buf.signature_help, opts)
-    vim.keymap.set('n', '<leader>do', '<cmd>lua vim.diagnostic.open_float()<CR>')
-    vim.keymap.set('n', '<leader>dp', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
-    vim.keymap.set('n', '<leader>dn', '<cmd>lua vim.diagnostic.goto_next()<CR>')
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "<leader>dj", vim.diagnostic.goto_next, opts)
+    vim.keymap.set("n", "<leader>dk", vim.diagnostic.goto_prev, opts)
+    vim.keymap.set("n", "<leader>df", "<cmd>Telescope diagnostics<cr>", opts)
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+    vim.keymap.set("n", "<C-h>", vim.lsp.buf.signature_help, opts)
+    vim.keymap.set("n", "<leader>do", "<cmd>lua vim.diagnostic.open_float()<CR>")
+    vim.keymap.set("n", "<leader>dp", "<cmd>lua vim.diagnostic.goto_prev()<CR>")
+    vim.keymap.set("n", "<leader>dn", "<cmd>lua vim.diagnostic.goto_next()<CR>")
 end
 
 lsp.ensure_installed({
   "tsserver",
-  "eslint",
   "sumneko_lua",
   "intelephense",
   "pyright"
