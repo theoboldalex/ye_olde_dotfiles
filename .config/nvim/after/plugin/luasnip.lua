8e488f5a8a956d2fbc474a08fe10172c8fce5503
theoboldalex <theoboldalex@gmail.com>
2023-05-01 17:45:21 +0100

    Felt cute, might delete later

---
+++ .config/nvim/after/plugin/luasnip.lua
@@ -0,0 +1,30 @@
+local ls = require("luasnip")
+
+vim.keymap.set({ "i", "s" }, "<C-k>", function ()
+    if ls.expand_or_jumpable() then
+        ls.expand_or_jump()
+    end
+end, { silent = true })
+
+vim.keymap.set({ "i", "s" }, "<C-j>", function ()
+    if ls.jumpable(-1) then
+        ls.jump(-1)
+    end
+end, { silent = true })
+
+vim.keymap.set("n", "<leader><leader>s", "<cmd>source ~/.config/nvim/after/plugin/luasnip.lua<cr>")
+
+-- snippets
+
+ls.add_snippets(
+    nil,
+    {
+        php = {
+            ls.parser.parse_snippet("vd", "var_dump($1);"),
+            ls.parser.parse_snippet("vdd", "var_dump($1); die();"),
+            ls.parser.parse_snippet("pubf", "public function $1($2): $3\n{\n\t$0\n}\n"),
+            ls.parser.parse_snippet("prof", "protected function $1($2): $3\n{\n\t$0\n}\n"),
+            ls.parser.parse_snippet("prif", "private function $1($2): $3\n{\n\t$0\n}\n"),
+        }
+    }
+)
